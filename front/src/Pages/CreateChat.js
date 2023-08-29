a3b3d0cfa7fe54a3ff46e06bb3d0d16569c1becd
JulienGha <julien.ghali@efrei.net>
2023-08-29 20:31:29 +0900

    implementation server

--- front/src/Pages/CreateChat.js
+++ front/src/Pages/CreateChat.js
@@ -7,24 +7,31 @@ const CreateChatPage = () => {
   const [password, setPassword] = useState('');
   const [chatId, setChatId] = useState('');
 
-  const handleCreateChat = () => {
-    // Send request to server for creating a new chat with password
-    // Server generates a unique chat ID and returns it
-    // Update the state with the generated chat ID
+  const handleCreateChat = async () => {
+    // Test function to see if the server is answering
+    try {
+      const response = await axios.post(`http://localhost:5000/create`);
+      setChatId(response.data.chatId)
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
   };
+
 
   return (
     <div>
       <Navbar></Navbar>
       <div>
-        <input
-          type="password"
-          placeholder="Set Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleCreateChat}>Create Chat</button>
-        {chatId && <p>Your Chat ID: {chatId}</p>}
+        <div>
+          <input
+            type="password"
+            placeholder="Set Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleCreateChat}>Create Chat</button>
+        </div>
       </div>
       <Footer></Footer>
     </div>
