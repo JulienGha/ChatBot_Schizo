d29fe48daec540cceb30e08499a756b4c3d2bbf1
JulienGha <julien.ghali@efrei.net>
2023-09-28 14:36:31 +0900

    logos

--- front/src/Components/Navbar.js
+++ front/src/Components/Navbar.js
@@ -3,22 +3,25 @@ import { Link } from 'react-router-dom';
 import axios from "axios";
 import "../Styles/Navbar.css"
 import "../Styles/AllStyle.css"
+import logo from "../Images/LOGO-JULIEN.png"
 
 const Navbar = () => {
 
     const handleCreateChat = async () => {
         try {
-          const response = await axios.get(`http://localhost:5000/test`);
-          alert(response.status)
+            const response = await axios.get(`http://localhost:5000/test`);
+            alert(response.status)
         } catch (error) {
-          console.log(error)
+            console.log(error)
         }
     };
 
     return (
         <div className="containerNavBar">
             <nav className="DivNavbar">
-                <Link to="/">Hermegency</Link>  {/* Updated link to home */}
+                <Link to="/">
+                    <img src={logo} alt="Hermegency logo" className="logoN"/>
+                </Link>
                 <button onClick={handleCreateChat}>Server test</button>
             </nav>
         </div>
