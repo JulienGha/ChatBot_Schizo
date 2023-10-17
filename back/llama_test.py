0bb8b3d506a4d51757e597301ef29d5264e866f0
JulienGha <julien.ghali@efrei.net>
2023-10-17 16:33:40 +0900

    probleme size gpu and depedencies version (tensorflowgpu)

--- back/llama_test.py
+++ back/llama_test.py
@@ -1,6 +1,6 @@
 # Import streamlit for app dev
 import streamlit as st
-
+import os
 # Import transformer classes for generation
 from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
 # Import torch for datatype attributes
@@ -20,18 +20,19 @@ from llama_index import ServiceContext
 from llama_index import VectorStoreIndex, download_loader
 from pathlib import Path
 import key
+# streamlit run C:\Users\JulienG\Documents\code\ChatBot_Schizo\back\llama_test.py
 
 # Define variable to hold llama2 weights naming
-name = "meta-llama/Llama-2-13b-chat-hf"
+name = "meta-llama/Llama-2-7b-chat-hf"
 # Set auth token variable from hugging face
 auth_token = key.auth_token
 
 
+# Check if a GPU is available
 if torch.cuda.is_available():
     # Set the current device to the GPU
-    print("set to gpu")
+    torch.cuda.empty_cache()
     torch.cuda.set_device(0)
-
 
 @st.cache_resource
 def get_tokenizer_model():
@@ -42,36 +43,29 @@ def get_tokenizer_model():
                                                  , use_auth_token=auth_token, torch_dtype=torch.float16,
                                                  rope_scaling={"type": "dynamic", "factor": 2},
                                                  device_map='auto')
-
     return tokenizer, model
 
 
 tokenizer, model = get_tokenizer_model()
 
+
 # Create a system prompt
 system_prompt = """<s>[INST] <<SYS>>
-You are a helpful, respectful and honest assistant. Always answer as 
-helpfully as possible, while being safe. Your answers should not include
-any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content.
-Please ensure that your responses are socially unbiased and positive in nature.
-
-If a question does not make any sense, or is not factually coherent, explain 
-why instead of answering something not correct. If you don't know the answer 
-to a question, please don't share false information.
-
-Your goal is to provide answers relating to the financial performance of 
-the company.<</SYS>>
+You are a helpful, respectful and honest assistant. 
+<</SYS>>
 """
 # Throw together the query wrapper
 query_wrapper_prompt = SimpleInputPrompt("{query_str} [/INST]")
 
-# Create a HF LLM using the llama index wrapper
-llm = HuggingFaceLLM(context_window=4096,
-                    max_new_tokens=256,
-                    system_prompt=system_prompt,
-                    query_wrapper_prompt=query_wrapper_prompt,
-                    model=model,
-                    tokenizer=tokenizer)
+llm = HuggingFaceLLM(
+    context_window=2048,
+    max_new_tokens=1024,
+    generate_kwargs={"temperature": 0.0, "do_sample": False},
+    query_wrapper_prompt=query_wrapper_prompt,
+    device_map="auto",
+    # change these settings below depending on your GPU
+    model_kwargs={"torch_dtype": torch.float16},
+)
 
 # Create and dl embeddings instance
 embeddings = LangchainEmbedding(
@@ -80,7 +74,7 @@ embeddings = LangchainEmbedding(
 
 # Create new service context instance
 service_context = ServiceContext.from_defaults(
-    chunk_size=1024,
+    chunk_size=512,
     llm=llm,
     embed_model=embeddings
 )
@@ -92,18 +86,20 @@ PyMuPDFReader = download_loader("PyMuPDFReader")
 # Create PDF Loader
 loader = PyMuPDFReader()
 # Load documents
-documents = loader.load(file_path=str(Path('cognitive_neuropsycho_schizo.pdf')), metadata=True)
+documents = loader.load(file_path=str(Path(
+    'C:/Users/JulienG/Documents/code/ChatBot_Schizo/back/cognitive_neuropsycho_schizo.pdf')), metadata=True)
 
 # Create an index - we'll be able to query this in a sec
 index = VectorStoreIndex.from_documents(documents)
 # Setup index query engine using LLM
 query_engine = index.as_query_engine()
+torch.cuda.empty_cache()
 
-# Create centered main title
-st.title('Llama')
+
 # Create a text input box for the user
 prompt = st.text_input('Input your prompt here')
-
+# Create centered main title
+st.title('Llama')
 # If the user hits enter
 if prompt:
     response = query_engine.query(prompt)
@@ -116,4 +112,3 @@ if prompt:
     # Display source text
     with st.expander('Source Text'):
         st.write(response.get_formatted_sources())
-
